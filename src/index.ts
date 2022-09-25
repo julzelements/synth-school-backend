94bc767fc9b9c80103394a912f8a311021c8bcd6
Julian Scharf <julian.scharf@tshirtventures.com.au>
2022-09-25 15:56:29 +1000

    blob is being saved

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors, { CorsOptions } from "cors";
+import multer from "multer";
 
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    console.log("filename");
+    cb(null, file.originalname);
+  },
+  destination: function (req, file, cb) {
+    console.log("storage");
+    cb(null, "./uploads");
+  },
+});
+const upload = multer({ storage });
 const prisma = new PrismaClient();
 const app = express();
 
@@ -16,10 +28,18 @@ app.use(cors(corsOptions));
 // TODO: PUT  	 /patches/{id}   : Update the patch information identified by "id"
 // TODO: DELETE	 /patches/{id}   : Delete patch by "id"
 
+app.post("/audio", upload.single("blob"), async (req, res) => {
+// app.post("/audio", async (req, res) => {
+  console.log(req);
+  console.log(req.body);
+  console.log(req.file);
+  res.status(200).send({ message: "Successfully uploaded files" });
+});
+
 app.get("/patches", async (req, res) => {
   const patches = await prisma.patch.findMany();
   console.log("GET/patches");
-  res.json(patches);
+  res.status(200).json(patches);
 });
 
 app.get("/patches/:id", async (req, res) => {
@@ -30,7 +50,7 @@ app.get("/patches/:id", async (req, res) => {
     },
   });
   console.log(`GET/patches/${id}`);
-  res.json(patches);
+  res.status(200).json(patches);
 });
 
 app.post("/patches", async (req, res) => {
@@ -39,10 +59,8 @@ app.post("/patches", async (req, res) => {
       name: req.body.name,
     },
   });
-  res.json(patch);
+  res.status(200).json(patch);
 });
-
-app.get("patches/");
 
 app.listen(3001, () =>
   console.log("REST API server ready at: http://localhost:3001")
